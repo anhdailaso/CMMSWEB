7182bba8fc46f4ecbc2ae5d1dce22d9fcbdf6bb1
AnNhanNguyen <nguyenannhan3107@gmail.com>
2023-02-13 22:59:38 +0700

    update upload file control

--- VietSoft.CMMS.Web/wwwroot/js/pages/multiple-file-upload.js
+++ VietSoft.CMMS.Web/wwwroot/js/pages/multiple-file-upload.js
@@ -1,32 +1,63 @@
 ﻿var MultipleFileUploadModule = MultipleFileUploadModule || (function () {
     function init() {
         $('#files').change(function (e) {
+           console.log( e.target.files)
             $(e.target.files).each(function () {
                 var file = this
                 var fileReader = new FileReader();
-
                 fileReader.readAsDataURL(file);
+                if (file.type === "image/png" || file.type === "image/jpeg") {
+                    $('.error').remove();
+                    fileReader.onload = function (e) {
+                        let src = e.target.result
 
-                fileReader.onload = function (e) {
-                    let src = e.target.result
-
-                    let html = ` 
-                          <div data-type="image-model" class="col-4 pl-2 pr-2 pt-2" style="max-width:200px;">
-                           <div class="ratio-box text-center" data-type="image-ratio-box">
+                        let html = ` 
+                          <div class="col-4 pl-2 pr-2 pt-2 image-model" style="max-width:200px;">
+                                <div class="remove-image" data-image-name="`+ file.name + `"><i class="fa fa-times" aria-hidden="true"></i></div>
+                                
+                                <div class="ratio-box text-center" data-type="image-ratio-box">
                                     <div data-type="loading" class="img-loading" style="color:#218838; display:none;">
                                       <span class="fa fa-2x fa-spin fa-spinner"></span>
                                     </div>
                                     <img data-type="preview" class="btn btn-light ratio-img img-fluid p-2 image border dashed rounded" src="`+ src + `" style="cursor: default;">
-                                    <span class="badge badge-pill badge-success p-2 w-50 main-tag" style="display:none;">Main</span>
                                   </div>
                         </div>
                         `
-                    $('#filesContent').find('.row').append(html)
+                        $('#filesContent').find('.row').append(html)
+                    }
+                }
+                else {
+
+                    let html = `<span class="error">Không đúng định dạng.</span>`
+                    $('.add-image').closest('.row').append(html)
+                    return false;
                 }
             })
 
-
         });
+        $(document).on('click', '.add-image', function () {
+            $('#files').click();
+        })
+        $(document).on('click', '.remove-image', function () {
+           
+            let fileName = $(this).data('image-name')
+            const dt = new DataTransfer()
+            const input = document.getElementById('files')
+            const { files } = input
+
+            for (let i = 0; i < files.length; i++) {
+                const file = files[i]
+                if (file.name != fileName) {
+                    console.log('a')
+                    dt.items.add(file)
+                }
+                    
+            }
+            input.files = dt.files // Assign the updates list
+            console.log(input.files)
+            $(this).closest('.image-model').remove();
+        })
+
     }
 
     return {
